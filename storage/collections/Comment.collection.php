eea6f15e3c2b21f70734fbc84d368347cbf8745b
Daniel Lindberg <daniel.lindberg.flash@gmail.com>
2019-09-27 21:05:39 +0200

    cockpit file

---
+++ storage/collections/Comment.collection.php
@@ -0,0 +1,114 @@
+<?php
+ return array (
+  'name' => 'Comment',
+  'label' => '',
+  '_id' => 'Comment5d815c068908a',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'Name',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'Text',
+      'label' => '',
+      'type' => 'textarea',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'Rating',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+        'type' => 'number',
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'Produkt',
+      'label' => '',
+      'type' => 'collectionlink',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+        'link' => 'Produkt',
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => false,
+  'in_menu' => false,
+  '_created' => 1568758790,
+  '_modified' => 1568760002,
+  'color' => '',
+  'acl' => 
+  array (
+    'public' => 
+    array (
+      'entries_view' => true,
+    ),
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+      'enabled' => false,
+    ),
+    'read' => 
+    array (
+      'enabled' => false,
+    ),
+    'update' => 
+    array (
+      'enabled' => false,
+    ),
+    'delete' => 
+    array (
+      'enabled' => false,
+    ),
+  ),
+);
